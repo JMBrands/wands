a7ef93633d9fa97116b8ca5617e848784e237d7e
JMBrands <jonar@ultramagic.net>
2023-07-13 16:42:00 +0200

    gui

--- src/client/java/org/eu/net/jmbrands/screens/AltarScreen.java
+++ src/client/java/org/eu/net/jmbrands/screens/AltarScreen.java
@@ -1,17 +1,14 @@
 package org.eu.net.jmbrands.screens;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 public class AltarScreen extends HandledScreen<AltarScreenHandler> {
     //A path to the gui texture. In this example we use the texture from the dispenser
-    private static final Identifier TEXTURE = new Identifier("minecraft", "textures/gui/container/dispenser.png");
-
+    private static final Identifier TEXTURE = new Identifier("wands", "textures/gui/altargui.png");
+    protected int backgroundHeight = 198;
     public AltarScreen(AltarScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
     }
@@ -34,6 +31,7 @@ public class AltarScreen extends HandledScreen<AltarScreenHandler> {
     protected void init() {
         super.init();
         // Center the title
+        titleY = 6 - (198-166)/2;
         titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2;
     }
 }
